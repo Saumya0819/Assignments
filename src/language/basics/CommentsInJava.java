135d35921a863ab942fb3b96c43f4dec037480c2
Saumya0819 <sranjan.hati@gmail.com>
2025-04-06 13:42:46 +0530

    Assignments

---
+++ src/language/basics/CommentsInJava.java
@@ -0,0 +1,10 @@
+package language.basics;
+
+public class CommentsInJava {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
---
+++ src/language/basics/FirstJavaClass.java
@@ -0,0 +1,40 @@
+package language.basics;
+
+import java.util.logging.Logger;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// Print the content and then go to next line
+	       System.out.println("Hello, World!");
+	       System.out.println("Hello, Mega!");
+	       
+	       // Print the content and then stay on same line
+	       System.out.print("Hello,");
+	       System.out.print(" World!");
+	       System.out.println();
+	       
+	       // Print the formated content and then stay on same line
+	       System.out.printf("%s is completed Btech, empId is %d and visa status is %b","Chandra",1236,false);
+	       System.out.println();
+	       
+	       // Format the digits and then Print the formated content and then stay on same line
+	       System.out.format("Total Bill is %.2f %n",100.93543539);
+	       System.out.println();
+	       
+	       // Join multiple print statements together
+	       System.out.append("My Name is Saumyaranjan").append(", My emp id is 1234");
+	       System.out.println();
+	       
+	       // Print the char based on ASCII value
+	       System.out.write(65);
+	       System.out.println();
+	    // Print Logs of Program
+	       Logger log = Logger.getLogger("MyLogger");
+	       log.info("Current line is 34");
+	       log.warning("There is some warning at line 34, Please check");
+
+	       
+	}
+
+}
