8b18f053971aeaa7868fc3dafc9803d0e7ab3773
Saumya0819 <sranjan.hati@gmail.com>
2025-04-09 09:25:43 +0530

    Assignment1 for Datatypes

---
+++ src/language/basics/Assignments_Datatypes.java
@@ -0,0 +1,81 @@
+package language.basics;
+
+public class Assignments_Datatypes {
+
+	public static void main(String[] args) {
+//Temperature of a city in degrees Celsius 25.5f -->(Datatype=Float)
+		float temperature = 25.5f;
+		System.out.println(temperature);
+// Whether a customer has placed an order: true or false -->(Datatype=boolean)
+		boolean hasPlacedOrder =true;
+		System.out.println("Has the customer placed an order? " + hasPlacedOrder);	
+		boolean hasPlacedOrder1 =false;
+		System.out.println("Has the customer placed an order? " + hasPlacedOrder1);
+//Person's phone number: "123-456-7890"-->(Datatype=string)
+		String phonenumber="123-456-7890";
+		System.out.println(phonenumber);
+//Amount of money in a Saumya's bank account: 1000.50-->(Datatype=double)
+		double bankbalance =1000.50;
+		System.out.println("Saumya's bank balance $" + bankbalance);
+//John's email address: "john.doe@example.com"-->(Datatype=String)	
+		String email = "john.doe@example.com";
+		System.out.println("John's emailId = "+email);
+//Coordinates of a location (latitude, longitude): 37.7749, -122.4194 -->(Datatype=double)		
+		double latitude = 37.7749;
+		double longitude = -122.4194;
+		System.out.println("latitude " + latitude);
+		System.out.println("longitude" + longitude);
+//Person's marital status: true or false -->(Datatype=boolean)
+		boolean maritalstatus = true;
+		boolean maritalstatus1 = false;
+		System.out.println("maritalstatus "+maritalstatus);
+		System.out.println("maritalstatus1 "+maritalstatus1);
+//Saumya's occupation: "Software Engineer"-->(Datatype=String)
+		String occupation = "Software Engineer";
+		System.out.println("Saumya's occupation = "+occupation);
+//Saumya's favourite colour: "Blue"	-->(Datatype=String)	
+		String colour = "Blue";
+		System.out.println("Saumya's favourite colour = "+colour);
+//Current year: 2023-->(Datatype=int)
+		int year = 2023;
+		System.out.println("Current year = "+year);
+//Number of followers on a social media platform: 1,000,000-->(Datatype=int)
+		int followers = 1000000;
+		System.out.println("Number of followers on a social media platform = "+followers);
+//Rating of a movie: 7.5-->(Datatype=float)		
+		float rating =7.5f;
+		System.out.println("Rating of a movie = "+rating);
+//VIKRAM's blood type: 'A'-->(Datatype=char)
+		char bloodtype='A';
+		System.out.println("VIKRAM's blood type = "+bloodtype);
+//Title of a book: "To Kill a Mockingbird"-->(Datatype= string)
+		String title = "To Kill a Mockingbird";
+		System.out.println("Title of a book = "+title);
+//Number of employees in a company: 500-->(Datatype=int)
+		int noofemployees =500;
+		System.out.println("Number of employees in a company = "+noofemployees);
+//Time of an event: 2:30 PM--> (Datatype=String)
+		String eventtime ="2:30 PM";
+		System.out.println("Time of an event = "+eventtime);
+//Name of a country: "United States"--> (Datatype=String)
+		String country = "United States";
+		System.out.println("Name of a country = "+country);
+//Ram's eye color: "Brown"--> (Datatype=String)
+		String color = "Brown";
+		System.out.println("Ram's eye color = "+color);
+//Arjun's birthplace: "New York City"--> (Datatype=String)
+		String birthplace = "New York City";
+		System.out.println("Arjun's birthplace = "+birthplace);
+//Distance between two cities: 200.5-->(Datatype=float)
+		float distance =200.5f;
+		System.out.println("Distance between two cities = "+distance);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- src/language/basics/CommentsInJava.java
+++ src/language/basics/CommentsInJava.java
@@ -1,9 +1,46 @@
 package language.basics;
 
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.util.logging.Logger;
+
 public class CommentsInJava {
 
 	public static void main(String[] args) {
-	
+		// Print the content and then go to next line
+	       System.out.println("Hello, World!");
+	       System.out.println("Hello, Mega!");
+	       
+	       // Print the content and then stay on same line
+	       System.out.print("Hello,");
+	       System.out.print(" World!");
+	       System.out.println();
+	       
+	       // Print the formated content and then stay on same line
+	       System.out.printf("%s is completed Btech, empId is %d and visa status is %b","Chandra",1236,false);
+	       System.out.println();
+	       
+	       // Format the digits and then Print the formated content and then stay on same line
+	       System.out.format("Total Bill is %.2f %n",100.93543539);
+	       System.out.println();
+	       
+	       // Join multiple print statements together
+	       System.out.append("My Name is Saumyaranjan").append(", My emp id is 1234");
+	       System.out.println();
+	       
+	       // Print the char based on ASCII value
+	       System.out.write(65);
+	       System.out.println();
+	    // Print Logs of Program
+	       Logger log = Logger.getLogger("MyLogger");
+	       log.info("Current line is 34");
+	       log.warning("There is some warning at line 34, Please check");
+      
+	       PrintWriter writer = new PrintWriter("C:\\Training\\Mar_2025_930PM\\AutomationTraining\\AutomationProject\\Logs.txt");
+	       writer.println(LocalDateTime.now()+"Currently Line 40 is running");
+	       writer.println(LocalDateTime.now()+"Currently Line 41 is running");
+	       writer.println(LocalDateTime.now()+"Execution is compled");
+	       writer.close();
 
 	}
 
